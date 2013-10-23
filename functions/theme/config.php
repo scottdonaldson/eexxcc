3c2dbb7750b2290228985c37bf96875b9701eebe
Scott Donaldson <scott.p.donaldson@gmail.com>
2013-10-23 09:06:58 -0400

    stripped down functions

--- functions/theme/config.php
+++
@@ -1,8 +0,0 @@
-<?php return array(
-/**
- * Theme configuration goes there
- */
-    'name'    => 'OriginMag',   						// theme name
-    'styled'  => false,           				 		// true if current theme supports styling
-    'scripts' => array( 'dropdown')			    		// javascripts theme uses
-);
