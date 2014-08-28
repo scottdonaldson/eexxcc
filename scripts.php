b729159f1b82430d9b226dad8592669e2526817e
Scott Donaldson <scott.p.donaldson@gmail.com>
2014-08-28 09:22:17 -0400

    v1.0.4

--- scripts.php
+++ scripts.php
@@ -25,7 +25,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-45464393-1', 'eexxcc.com');
+  ga('create', 'UA-45464393-1', 'extracurriculars.us');
   ga('send', 'pageview');
 
 </script>
